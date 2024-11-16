d37638e1b2838daf32b35eff771b57db1839f5ff
Harshit Bole <harshitdon3@gmail.com>
2024-11-16 21:13:18 +0530

    Added Threads For Comments & Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,10 +7,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://8de650774a35ddbc3a9800438bc54566@o4506658653011968.ingest.us.sentry.io/4508308413808640",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
